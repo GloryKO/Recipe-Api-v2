209b1df16482d5c1fd0e80f6cca2e7641f52c7e4
GloryKO <koladeg2022@gmail.com>
2023-11-13 00:01:47 +0100

    added user creation test pass

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -8,7 +8,6 @@ class ModelTestCase(TestCase):
     def test_create_user_with_email_success(self):
         email='test@example.com'
         password= 'testpass123'
-        user =get_user_model().objects.create(email=email,password=password)
+        user =get_user_model().objects.create_user(email=email,password=password)
         self.assertEqual(user.email,email)
-        self.assertEqual(user.check_password(password))
-        
+        self.assertTrue(user.check_password(password))
