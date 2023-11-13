16aec122b090a0ef125940fda2aa4826cd006401
GloryKO <koladeg2022@gmail.com>
2023-11-13 08:28:41 +0100

    passed user list in admin page

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -17,5 +17,7 @@ class AdminSiteTests(TestCase):
         self.user = get_user_model().objects.create_user(email='testuser@example.com',password='testpass123',name='test user')
     
     def test_users_list(self):
-        url = reverse('admin:core_user_change_list')
+        url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
+        self.assertContains(res,self.user.name)
+        self.assertContains(res,self.user.email)
