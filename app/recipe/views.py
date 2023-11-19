91f793c528e12bb023ebdfc45aa8707b3320c095
GloryKO <koladeg2022@gmail.com>
2023-11-19 11:13:21 +0100

    passed test for listing tags

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -8,9 +8,10 @@ from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe
+from core.models import Recipe,Tag
 from recipe import serializers
 
+from rest_framework import viewsets,mixins
 
 class RecipeViewSet(viewsets.ModelViewSet):
     """View for manage recipe APIs."""
@@ -30,3 +31,13 @@ class RecipeViewSet(viewsets.ModelViewSet):
     
     def perform_create(self,serializer):
         serializer.save(user=self.request.user)
+
+class TagViewSet(mixins.ListModelMixin,viewsets.GenericViewSet):
+    serializer_class =serializers.TagSerializer
+    queryset = Tag.objects.all()
+    authentication_classes=[TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Filter queryset to authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
