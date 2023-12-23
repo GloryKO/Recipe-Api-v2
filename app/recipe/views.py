1c1ed17722723e9b45118d808c5308d141f091c2
GloryKO <koladeg2022@gmail.com>
2023-12-23 10:40:40 +0100

    refactored the viewsets for tags and ingredients

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -31,23 +31,20 @@ class RecipeViewSet(viewsets.ModelViewSet):
     
     def perform_create(self,serializer):
         serializer.save(user=self.request.user)
-
-class TagViewSet(mixins.DestroyModelMixin,mixins.ListModelMixin,mixins.UpdateModelMixin,viewsets.GenericViewSet):
-    serializer_class =serializers.TagSerializer
-    queryset = Tag.objects.all()
-    authentication_classes=[TokenAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        """Filter queryset to authenticated user."""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
-
-class IngredientViewSet(mixins.DestroyModelMixin,mixins.UpdateModelMixin,mixins.ListModelMixin,viewsets.GenericViewSet):
-    serializer_class = serializers.IngredientSerializer
-    queryset = Ingredient.objects.all()
+        
+class BaseRecipeAttrViewSet(mixins.DestroyModelMixin,mixins.UpdateModelMixin,mixins.ListModelMixin,viewsets.GenericViewSet):
+    """Base viewset for recipe attributes."""
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
-
     def get_queryset(self):
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+class TagViewSet(BaseRecipeAttrViewSet):
+    serializer_class =serializers.TagSerializer
+    queryset = Tag.objects.all()
+
+class IngredientViewSet(BaseRecipeAttrViewSet):
+    serializer_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
+
     
